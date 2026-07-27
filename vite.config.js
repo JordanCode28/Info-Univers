082626d9af84f8cd3be54087767babbaa97f31ed
JordanCode <christjordan53@gmail.com>
2026-07-27 23:04:49 +0100

    correction: build multi-page et la base pour github pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
-export default {
+import { defineConfig } from "vite"
+
+export default defineConfig({
     base: '/Info-Univers/',
-}
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                annuaire: 'annuaire.html',
+                apropos: 'apropos.html',
+                contact: 'contact.html',
+                detail: 'detail.html',
+            }
+        }
+    }
+})
